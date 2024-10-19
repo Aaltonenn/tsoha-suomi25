8d2343787bac1787dc292efaa1b2b8791f11d95f
Aaltonen <ajoona@cubbli24-gold.cs.helsinki.fi>
2024-10-19 14:30:38 +0300

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE comments (
+	id SERIAL PRIMARY KEY, 
+	userid INTEGER,
+	threadid INTEGER,
+	time TIMESTAMP,
+	content TEXT,
+	username TEXT
+);
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY, 
+	username TEXT,
+	password TEXT,
+	admin INTEGER
+);
+
+CREATE TABLE follows (
+	id SERIAL PRIMARY KEY, 
+	userid INTEGER,
+	threadid INTEGER
+);
+
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY, 
+	content TEXT
+);
+
+CREATE TABLE subjectarea (
+	id SERIAL PRIMARY KEY, 
+	subjectarea TEXT,
+	shortname TEXT
+);
+
+CREATE TABLE threads (
+	id SERIAL PRIMARY KEY, 
+	userid INTEGER,
+	subjectarea INTEGER,
+	title TEXT,
+	content TEXT
+);
