f5b2509880f0610249882a509fb5127c8a38505a
Aaltonen <ajoona@cubbli24-gold.cs.helsinki.fi>
2024-10-20 19:58:38 +0300

    modified requirements

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-
+import os
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql+psycopg2://')
+
 
 
 import routes
